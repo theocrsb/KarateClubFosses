8281c74aeb8ae09cc0a2e8c52af661ca849285ab
theocrsb <theo_crosbie@yahoo.fr>
2022-12-01 22:18:08 +0100

    init calendar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
+import React from 'react'
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
 const Home = () => {
+ const handleDateClick = (arg) => { // bind with an arrow function
+    alert(arg.dateStr)
+  }
   return (
     <div className="bg-grey">
+       <FullCalendar
+        plugins={[ dayGridPlugin ]}
+        initialView="dayGridMonth"
+        weekends={false}
+        events={[
+          { title: 'cours Karaté jeudi', date: '2022-12-01' },
+          { title: 'cours Karaté lundi', date: '2022-12-05' },
+        ]}
+        dateClick={handleDateClick}
+      />
     </div>
   );
 };
